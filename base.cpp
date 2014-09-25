38494915a2b5bc50e7234b478c9fba6ff0664336
Ryuno <bf_1706@yahoo.co.jp>
2014-09-25 21:56:24 +0900

    add base.cpp

---
+++ base.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdlib>
+#include <deque>
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+#define rep(i,l) for(int(i)=0; (i)<int(l); i++)
+#define erep(i,l,j,x) for(int(i)=int(l); (i)<int(j); i+=x)
+#define mp(x,y) make_pair(x, y)
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define pb(x) push_back(x)
+#define pob(x) pop_back(x)
+#define pf(x) push_front(x)
+#define pof(x) pop_front(x)
+#define srtg(x) sort(all(x), greater<int>())
+#define print(x) cout<<x<<endl;
+#define INF 0x3f3f3f3f
+#define INFL 0x3f3f3f3f3f3f3f3fLL
+using namespace std;
+typedef vector<int> vi; typedef vector<string> vs; typedef vector<double> vd;
+typedef pair<int, int> pi;
+typedef priority_queue<int> pqi; typedef priority_queue<int, vector<int>, greater<int> > pqig;
+
+
